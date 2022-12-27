00bb37fa6baf5a06718dac9ed35a08faf546703c
foxthehuman <foxthehuman1900@gmail.com>
2022-12-27 14:03:40 +0700

    a

--- src/app.js
+++ src/app.js
@@ -54,5 +54,6 @@ app.use(routes);
 //cron.start();
 
 app.listen(PORT, HOST, () => {
+  console.log("TEST ", process.env.HOST, process.env.DB_USER, process.env.PASSWORD, process.env.DATABASE_NAME);
   console.log(`Server is running on port: ${PORT} ${HOST}`);
 });
